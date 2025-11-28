73acc849db597067894ea3a053086e2d753aef39
rkolay35-blip <rkolay35@gmail.com>
2025-11-28 17:39:15 +0300

    Create db.js

---
+++ data/db.js
@@ -0,0 +1,61 @@
+export default {
+  "yemekler": [
+    {
+      "id": 1,
+      "isim": "Adana Kebap",
+      "resim": "https://cdn.yemek.com/mnresize/1250/833/uploads/2016/05/ev-usulu-adana-kebap-yemekcom.jpg",
+      "kategori": "et",
+      "sure": "45 dk",
+      "kalori": 360,
+      "malzemeler": ["Kıyma", "Kuyruk yağı", "Pul biber", "Kapya biber"],
+      "tarif": "Kıymayı ve yağı iyice yoğurun. Şişlere saplayıp ızgarada pişirin."
+    },
+    {
+      "id": 2,
+      "isim": "Mercimek Çorbası",
+      "resim": "https://cdn.yemek.com/mnresize/1250/833/uploads/2014/06/mercimek-corbasi-yemekcom.jpg",
+      "kategori": "corba",
+      "sure": "30 dk",
+      "kalori": 180,
+      "malzemeler": ["Kırmızı mercimek", "Soğan", "Havuç", "Patates"],
+      "tarif": "Sebzeleri kavurun, mercimeği ekleyin. Kaynayınca blendırdan geçirin."
+    },
+    {
+      "id": 3,
+      "isim": "Fırın Sütlaç",
+      "resim": "https://cdn.yemek.com/mnresize/1250/833/uploads/2014/08/sutlac-yemekcom.jpg",
+      "kategori": "tatli",
+      "sure": "50 dk",
+      "kalori": 250,
+      "malzemeler": ["Süt", "Pirinç", "Şeker", "Nişasta"],
+      "tarif": "Sütlacı pişirin, güveç kaplarına koyun. Fırının sadece üstünü açıp kızartın."
+    },
+    {
+      "id": 4,
+      "isim": "Zeytinyağlı Sarma",
+      "resim": "https://cdn.yemek.com/mnresize/1250/833/uploads/2014/11/zeytinyagli-yaprak-sarma-yemekcom.jpg",
+      "kategori": "zeytinyagli",
+      "sure": "60 dk",
+      "kalori": 300,
+      "malzemeler": ["Asma yaprağı", "Pirinç", "Soğan", "Baharatlar"],
+      "tarif": "İç harcı hazırlayıp yapraklara sarın. Limonlu suda kısık ateşte pişirin."
+    },
+    {
+      "id": 5,
+      "isim": "Mantı",
+      "resim": "https://cdn.yemek.com/mnresize/1250/833/uploads/2014/06/manti-yemekcom.jpg",
+      "kategori": "hamur",
+      "sure": "75 dk",
+      "kalori": 400,
+      "malzemeler": ["Un", "Kıyma", "Yoğurt", "Sarımsak"],
+      "tarif": "Hamuru açıp kıymalı harçla kapatın. Haşlayıp yoğurtla servis yapın."
+    }
+  ],
+  "kategoriler": [
+    { "id": "et", "baslik": "Et Yemekleri" },
+    { "id": "corba", "baslik": "Çorbalar" },
+    { "id": "tatli", "baslik": "Tatlılar" },
+    { "id": "zeytinyagli", "baslik": "Zeytinyağlılar" },
+    { "id": "hamur", "baslik": "Hamur İşleri" }
+  ]
+};
