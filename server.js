d056827e72d84e727e57ec55178e4076112a6382
rkolay35-blip <rkolay35@gmail.com>
2025-11-28 17:30:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const db = require('./data/db.json');
+const db = require('./data/yemekler.js');
 
 app.get('/', (req, res) => {
   res.send('Listify Yemek API');
